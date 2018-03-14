7963194a44d15006a463c3a8ae6c78a7106f58a8
Daniel Van Gulla <daniel.gulla88@gmail.com>
2018-03-14 17:18:30 +0800

    update

--- resources/views/content/kotamanado.blade.php
+++ resources/views/content/kotamanado.blade.php
@@ -61,7 +61,7 @@
 			
 				<div class="page foldtl " style="width: 11vw;">
 					
-				  <div class="row " style="margin-left:-18px; padding-right:10px; color:black;">
+				  <div class="row " style="margin-left:-18px; padding-right:10px; color:black; ">
 					  <h2 style="font-size:20px; margin-top:-20px;" class="text-center">Data <br>Kemiskinan<br>Kota Manado</h2>
 					  
 				  </div>
